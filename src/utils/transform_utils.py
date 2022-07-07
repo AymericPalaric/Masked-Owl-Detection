807ecfd39702ea6d52beeb2bfdf2e31721693158
Paul Borne--Pons <paulbp_dty@pauls-mbp.home>
2022-07-07 15:17:28 +0200

    mel

--- src/utils/transform_utils.py
+++ src/utils/transform_utils.py
@@ -9,4 +9,4 @@ def baseline_transform(data):
 def compute_mel_spectrogram(data: np.ndarray, fs: int, n_mels: int, n_fft: int, hop_length: int) -> np.ndarray:
   mel_spectrogram = librosa.feature.melspectrogram(y=data, sr=fs, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels, fmin=0, fmax=fs//2)
   mel_spectrogram = librosa.display.specshow(librosa.power_to_db(mel_spectrogram))
-  return mel_spectrogram
+  return np.stack((mel_spectrogram)*3, axis=0)
