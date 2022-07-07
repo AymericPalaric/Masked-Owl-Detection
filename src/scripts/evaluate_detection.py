5a313b87a835e79e9b576241ff6536558dcc40c1
Palaric Aymeric <aymeric.palaric@student-cs.fr>
2022-07-07 09:33:47 +0200

    wrong script

--- src/scripts/evaluate_detection.py
+++ src/scripts/evaluate_detection.py
@@ -16,51 +16,73 @@ import numpy as np
 
 if __name__ == "__main__":
 
-  classification_model = EfficientNet()
-  classification_model.load_state_dict(torch.load("trained_models/efficientnet_nosoft_19.pt"))
-  classification_model.eval()
+    classification_model = EfficientNet()
+    classification_model.load_state_dict(torch.load(
+        "trained_models/efficientnet_nosoft_19.pt"))
+    classification_model.eval()
 
-  window_size = int(22000 * 1)
-  window_overlap = window_size // 50
-  window_type = "boxcar"
-  reshape_size = (129, 129)
-  audio_transform = transform_utils.baseline_transform
-  image_transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Resize(reshape_size), torchvision.transforms.Normalize(mean=constants.mean_test, std=constants.std_test)])
+    window_size = int(22000 * 1)
+    window_overlap = window_size // 50
+    window_type = "boxcar"
+    reshape_size = (129, 129)
+    audio_transform = transform_utils.baseline_transform
+    image_transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Resize(
+        reshape_size), torchvision.transforms.Normalize(mean=constants.mean_test, std=constants.std_test)])
 
-  freq_max = 12000
-  treshold_nms = 0.5
-  device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-  pipeline = sliding_window.SlidingWindowPipeline(window_size, window_overlap, window_type, classification_model, audio_transform, image_transform, freq_max, treshold_nms, device)
+    freq_max = 12000
+    treshold_nms = 0.5
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    pipeline = sliding_window.SlidingWindowPipeline(
+        window_size, window_overlap, window_type, classification_model, audio_transform, image_transform, freq_max, treshold_nms, device)
 
-  time_scale = 1 / (256 - 256//4)
+    time_scale = 1 / (256 - 256//4)
 
-  files = [file[:-4] for file in os.listdir("data/detection_samples/samples") if file.endswith(".wav")][:5]
-  audios = list()
-  img_target = list()
-  bbxs_target = list()
-  for k, file in enumerate(files):
-    audio_path = os.path.join("data/detection_samples/samples", f"{file}.wav")
-    bbxs_path = os.path.join("data/detection_samples/targets", f"{file}.npy")
-    audio, _ = audio_utils.load_audio_file(audio_path)
-    bbxs = np.load(bbxs_path)
+    files = [file[:-4]
+             for file in os.listdir("data/detection_samples/samples") if file.endswith(".wav")][:5]
+    audios = list()
+    img_target = list()
+    bbxs_target = list()
+    for k, file in enumerate(files):
+        audio_path = os.path.join(
+            "data/detection_samples/samples", f"{file}.wav")
+        bbxs_path = os.path.join(
+            "data/detection_samples/targets", f"{file}.npy")
+        audio, _ = audio_utils.load_audio_file(audio_path)
+        bbxs = np.load(bbxs_path)
 
-    audios.append(audio)
+        audios.append(audio)
 
-    for bbx in bbxs:
-      if bbx[0] == 1:
-        img_target.append(k)
-        bbxs_target.append([int(bbx[1] * time_scale), 0, int(bbx[2] * time_scale), freq_max])
-  cropping_len = min([len(audio) for audio in audios])
-  audios = [audio[:cropping_len] for audio in audios]
-  audios = np.stack(audios)
-  img_target = torch.tensor(img_target, dtype=torch.long)
-  bbxs_target = torch.tensor(bbxs_target, dtype=torch.long)
+        for bbx in bbxs:
+            if bbx[0] == 1:
+                img_target.append(k)
+                bbxs_target.append(
+                    [int(bbx[1] * time_scale), 0, int(bbx[2] * time_scale), freq_max])
+    cropping_len = min([len(audio) for audio in audios])
+    audios = [audio[:cropping_len] for audio in audios]
+    audios = np.stack(audios)
+    img_target = torch.tensor(img_target, dtype=torch.long)
+    bbxs_target = torch.tensor(bbxs_target, dtype=torch.long)
 
-  #img_pred, bbxs_pred, score_pred = pipeline(audios)
-  #print(bbxs_pred)
-  #print(10*"=")
-  #print(score_pred)
-  #mAP = metrics_utils.compute_mAP((img_target, bbxs_target), (img_pred, bbxs_pred, score_pred), 0.5)
+    #img_pred, bbxs_pred, score_pred = pipeline(audios)
+    # print(bbxs_pred)
+    # print(10*"=")
+    # print(score_pred)
+    #mAP = metrics_utils.compute_mAP((img_target, bbxs_target), (img_pred, bbxs_pred, score_pred), 0.5)
 
-  #print("mAP: ", mAP)
+    #print("mAP: ", mAP)
+    file = files[0]
+    audio, fs = audio_utils.load_audio_file(os.path.join(
+        "data/detection_samples/samples", f"{file}.wav"))
+    lbl = np.load(os.path.join(
+        "data/detection_samples/targets", f"{file}.npy"))
 
+    bbxs, scores = pipeline(audio)
+
+    ts, f, spectro = audio_utils.compute_spectrogram(
+        audio, fs, nperseg=256, noverlap=256//4, scale="dB")
+    print(spectro.shape)
+    fig, ax = plt.subplots(1, 1, figsize=(100, 20))
+    ax.imshow(spectro)
+    # for box in bbxs:
+
+    #   ax.add_patch(plt.Rectangle())
